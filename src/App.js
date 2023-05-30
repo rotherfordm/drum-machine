ce83ecbeab9c4f2871aa62d456594cc5f8ea3633
Rotherford <rmarmibe@protonmail.com>
2023-05-30 15:16:09 +0800

    conditionals for mode

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { VolumeBar } from "./components/VolumeBar";
 import tiles from "./components/constants/tiles";
 import Elements from "./components/constants/elements";
 import { Tile } from "./components/Tile";
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from './components/constants/global';
-import  Toggle  from './components/Toggle';
-import { darkTheme, lightTheme, drumMode, pianoMode } from "./components/constants/theme";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/constants/global";
+import Toggle from "./components/Toggle";
+import {
+  darkTheme,
+  lightTheme,
+  drumMode,
+  pianoMode,
+} from "./components/constants/theme";
 import Buttons from "./components/Buttons";
 
 const elements = Elements();
@@ -20,7 +25,7 @@ const App = () => {
   const [charKey, setCharKey] = useState("");
   const [volume, setVolume] = useState(25);
   const [session, setSession] = useState([]);
-  const [mode, setMode] = useState('drum');
+  const [mode, setMode] = useState("drum");
   //const [theme, setTheme] = useState('light');
 
   /*const toggleTheme = () => {
@@ -35,108 +40,114 @@ const App = () => {
 
   const toggleMode = () => {
     // if the theme is not drum, then set it to piano
-    if (mode === 'piano') {
-      setMode('drum');
-    // otherwise, it should be piano
+    if (mode === "piano") {
+      setMode("drum");
+      // otherwise, it should be piano
     } else {
-      setMode('piano');
+      setMode("piano");
     }
-  }
+  };
 
   return (
-    <ThemeProvider 
-    /*theme={theme === 'light' ? lightTheme : darkTheme}*/
-    theme={mode === 'drum' ? drumMode : pianoMode}
-  >
+    <ThemeProvider
+      /*theme={theme === 'light' ? lightTheme : darkTheme}*/
+      theme={mode === "drum" ? drumMode : pianoMode}
+    >
       <GlobalStyles />
       {/* Toggle Theme */}
       <div>
-        <Toggle 
-        className="toggle" 
-        /*theme={theme} toggleTheme={toggleTheme} */
-        mode={mode} toggleMode={toggleMode}/>
-        </div>
-    <div
-      onKeyDown={(e) => {
-        const item = elements.find((x) => x.key === e.key.toLowerCase());
-        if (item) {
-          document.querySelector(item.id).play();
-          setCharKey(item.color);
-          setPressed("enabled");
-          if (isRecording) {
-            setSession([...session, item.key]);
-          } else {
-            setDisplayText(item.displayText);
-          }
-        }
-      }}
-      onKeyUp={() => {
-        setCharKey("");
-        setPressed("disabled");
-      }}
-    >
-      <div id="drum-machine">
-        {isRecording
-          ? "Recording"
-          : isPlaying
-          ? "Playing"
-          : "Start Making Sound"}
-        <br></br>
-        <div className="flex">
-          {/* Display */}
-          <div className="toolbar" id="display">
-            {displayText}
-          </div>
-          {/* Buttons */}
-          <Buttons 
-          setIsPlaying={setIsPlaying}
-          isPlaying={isPlaying}
-          setIsRecording={setIsRecording}
-          isRecording={isRecording}
-          setSession={setSession}
-          session={session}
-          setDisplayText={setDisplayText}
-          setPower={setPower}
-          power={power}
-          setVolume={setVolume}
-          setPressed={setPressed}
-          pressed={pressed}
-          setCharKey={setCharKey}
-          />
-          {/* VolumeBar */}
-          <VolumeBar
-            volumeValue={volume}
-            setDisplayText={setDisplayText}
-            setPower={setPower}
-            setVolume={setVolume}
-
-          />
-        </div>
-
-        {/* Drum keys */}
-        <div className="drum-keys">
-          {tiles.map((item, index) => {
-            return (
-              <Tile
-                key={index}
-                id={item.id}
-                charKey={charKey}
-                colorString={item.colorString}
-                display={item.displayText}
-                setDisplayText={setDisplayText}
-                pressed={pressed}
-                keyChar={item.keyChar}
-                audioSource={item.audioSource}
-                isRecording={isRecording}
-                session={session}
-                setSession={setSession}
-              />
-            );
-          })}
-        </div>
-        
+        <Toggle
+          className="toggle"
+          /*theme={theme} toggleTheme={toggleTheme} */
+          mode={mode}
+          toggleMode={toggleMode}
+        />
       </div>
-    </div>
+      <div
+        onKeyDown={(e) => {
+          const item = elements.find((x) => x.key === e.key.toLowerCase());
+          if (item) {
+            document.querySelector(item.id).play();
+            setCharKey(item.color);
+            setPressed("enabled");
+            if (isRecording) {
+              setSession([...session, item.key]);
+            } else {
+              setDisplayText(item.displayText);
+            }
+          }
+        }}
+        onKeyUp={() => {
+          setCharKey("");
+          setPressed("disabled");
+        }}
+      >
+        <div id="drum-machine">
+          {isRecording
+            ? "Recording"
+            : isPlaying
+            ? "Playing"
+            : "Start Making Sound"}
+          <br></br>
+          <div className="flex">
+            {/* Display */}
+            <div className="toolbar" id="display">
+              {displayText}
+            </div>
+            {/* Buttons */}
+
+            {mode === "drum" ? (
+              <Buttons
+                setIsPlaying={setIsPlaying}
+                isPlaying={isPlaying}
+                setIsRecording={setIsRecording}
+                isRecording={isRecording}
+                setSession={setSession}
+                session={session}
+                setDisplayText={setDisplayText}
+                setPower={setPower}
+                power={power}
+                setVolume={setVolume}
+                setPressed={setPressed}
+                pressed={pressed}
+                setCharKey={setCharKey}
+              />
+            ) : null}
+
+            {mode === "piano" ? <>piano</> : null}
+
+            {/* VolumeBar */}
+            <VolumeBar
+              volumeValue={volume}
+              setDisplayText={setDisplayText}
+              setPower={setPower}
+              setVolume={setVolume}
+            />
+          </div>
+
+          {/* Drum keys */}
+          <div className="drum-keys">
+            {tiles.map((item, index) => {
+              return (
+                <Tile
+                  key={index}
+                  id={item.id}
+                  charKey={charKey}
+                  colorString={item.colorString}
+                  display={item.displayText}
+                  setDisplayText={setDisplayText}
+                  pressed={pressed}
+                  keyChar={item.keyChar}
+                  audioSource={item.audioSource}
+                  isRecording={isRecording}
+                  session={session}
+                  setSession={setSession}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </ThemeProvider>
   );
 };
