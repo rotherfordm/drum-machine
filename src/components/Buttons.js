c226a586ab848cf87a0e1f82fbcaea63a267a193
Rotherford <22748659+rotherfordm@users.noreply.github.com>
2023-06-01 23:18:48 +0800

    Revert "take it back"
    This reverts commit 6d1ff8f260d93a0ab4a74ef50b8950ad24b25f04, reversing
    changes made to b0f46b3e006cd823fd59560994181e50e87ec03c.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { volumeControl } from "./Tile";
-import { tunes } from "./constants/tunes";
+import { tunes } from "./constants/tunes"
 
 export const DrumButtons = (props) => {
   const startRecording = () => {
@@ -28,35 +28,90 @@ export const DrumButtons = (props) => {
   const replayRecording = () => {
     props.setIsRecording(false);
     props.setIsPlaying(true);
+    for (var index = 0; index < props.session.length; index++) {
+      let item = props.session[index].toUpperCase();
+      setTimeout(() => {
+        document.getElementById(item).play();
+        props.setCharKey(item.toLowerCase());
+        props.setPressed("enabled");
+        if (index === props.session.length - 1) {
+          setTimeout(() => {
+            props.setCharKey("");
+            props.setPressed("disabled");
+            props.setDisplayText("");
+            props.setIsPlaying(false);
+          }, 450 * index);
+        }
+      }, 450 * index);
+    }
+    props.setSession([]);
   };
 
   return (
     <div>
-      <button className="record toolbar" onClick={startRecording}>
-        <i className="fa fa-record-vinyl" />
-      </button>
-      <button className="on-off toolbar" onClick={handlePowerOnOff}>
-        <i className="fa fa-power-off" />
-      </button>
-      <button className="replay toolbar" onClick={replayRecording}>
-        <i className="fa fa-play" />
-      </button>
-    </div>
-  );
+    <button className="record toolbar" onClick={startRecording}>
+    <i className="fa fa-record-vinyl" />
+  </button>
+  <button className="on-off toolbar" onClick={handlePowerOnOff}>
+    <i className="fa fa-power-off" />
+  </button>
+  <button className="replay toolbar" onClick={replayRecording}>
+    <i className="fa fa-play" />
+  </button>
+  </div>
+  )
 };
 
 export const PianoButtons = (props) => {
   const playTune = () => {
     props.setIsPlaying(true);
-    props.setSession([]);
     let randomTune = tunes[Math.floor(Math.random() * (tunes.length - 0) + 0)];
-    props.setSession([...props.session, ...randomTune.notes]);
-    props.setDisplayText(randomTune.title);
-  };
+    console.log(randomTune);
+    let randomTuneName = randomTune[0];
+    props.setDisplayText(randomTuneName);
+    for (var first = 1; first < randomTune.length; first += 2) {
+      var second = first + 1;
+      let firstNote = randomTune[first];
+      console.log(firstNote)
+      let secondNote = randomTune[second];
+      console.log(secondNote)
+      setTimeout(() => {
+        setTimeout(() => {
+        document.getElementById(firstNote.toLowerCase()).play();
+        props.setCharKey(firstNote);
+        props.setPressed("enabled");
+        if (firstNote === randomTune.length - 1) {
+          setTimeout(() => {
+            props.setCharKey("");
+            props.setPressed("disabled");
+            props.setIsRecording(false);
+            props.setDisplayText("");
+            props.setIsPlaying(false);
+          }, 2000 * firstNote);
+        } 600 * firstNote }),
+        document.getElementById(secondNote.toLowerCase()).play();
+        props.setCharKey(secondNote.toLowerCase());
+        props.setPressed("enabled");
+        if (secondNote === randomTune.length - 1) {
+          setTimeout(() => {
+            props.setCharKey("");
+            props.setPressed("disabled");
+            props.setIsRecording(false);
+            props.setDisplayText("");
+            props.setIsPlaying(false);
+          }, 2000 * secondNote);
+        }
+      },  600 * firstNote);
+      
+    }
+    props.setSession([]);
+
+
+  }
 
   return (
     <button className="toolbar" onClick={playTune}>
-      <i className="fa fa-music" />
+      <i className="fa fa-music"/>
     </button>
-  );
-};
+  )
+}
